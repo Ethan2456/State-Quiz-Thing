804b3539ade3533e00afb4e285514c785df03f53
Ethan Tran <etra@730-ln11701-r.jeffco.schools>
2023-11-01 09:37:39 -0600

    Added the actual code

--- fun.cpp
+++ fun.cpp
@@ -1,1 +1,26 @@
-//hi my name is daniel
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int choice;
+    cout << "Welcome to the US Geography Game!" << endl;
+    cout << "Which mode would you like to play?" << endl;
+    cout << "Capitols(1) or Cities(2)?" << endl;
+    cin >> choice;
+
+    if(choice == 1){
+        cout << "You chose the Capitol Option!" << endl;
+    }
+    if(choice == 2){
+        cout << "You chose the Cities Option!" << endl;
+    }
+
+
+
+
+
+
+return 0;
+}
