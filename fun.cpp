73fa7ad5cfb4099ddcee78c6e5bf455f4462d631
Ethan Tran <etra@730-ln11701-r.jeffco.schools>
2023-11-08 08:38:16 -0700

    die.

--- fun.cpp
+++ fun.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 
+string states(){
+    string cap1[] = {"Alabama", "Alaska", "Arizona", "Arkansas","California","Colorado","Connecticut","Delaware","Florida",
+                      "Georgia","Hawaii", "Idaho", "Illnois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts"
+                        "Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico",
+                        "New York", "North Carolina","North Dakota","Ohio","Oklahoma","Pennsylvania","Rhode Island","South Carolina","South Dakota",
+                        "Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"}
+}
+
+
+
+
 int main(){
     int choice;
     cout << "Welcome to the US Geography Game!" << endl;
