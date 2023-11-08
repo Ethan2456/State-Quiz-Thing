1d5972a51a888ef6426c3e52cc052c167805119e
Ethan Tran <etra@730-ln11701-r.jeffco.schools>
2023-11-08 09:04:39 -0700

    something

--- fun.cpp
+++ fun.cpp
@@ -8,38 +8,35 @@ States quiz.
 
 using namespace std;
 
-
-string states(){
-    string cap1[] = {"Alabama", "Alaska", "Arizona", "Arkansas","California",
-            "Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii",
-            "Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky",
-            "Louisiana","Maine","Maryland","Massachusetts","Michigan",
-            "Minnesota","Mississippi","Missouri","Montana","Nebraska",
-            "Nevada","New Hampshire","New Jersey","New Mexico",
-            "New York", "North Carolina","North Dakota","Ohio","Oklahoma",
-            "Pennsylvania","Rhode Island","South Carolina","South Dakota",
-            "Tennessee","Texas","Utah","Vermont","Virginia","Washington",
-            "West Virginia","Wisconsin","Wyoming"}
-}
-
-
-
-
 int main(){
+    string cap1[] = {"Alabama", "Alaska", "Arizona", "Arkansas","California",
+                "Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii",
+                "Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky",
+                "Louisiana","Maine","Maryland","Massachusetts","Michigan",
+                "Minnesota","Mississippi","Missouri","Montana","Nebraska",
+                "Nevada","New Hampshire","New Jersey","New Mexico",
+                "New York", "North Carolina","North Dakota","Ohio","Oklahoma",
+                "Pennsylvania","Rhode Island","South Carolina","South Dakota",
+                "Tennessee","Texas","Utah","Vermont","Virginia","Washington",
+                "West Virginia","Wisconsin","Wyoming"};
+
     int choice;
     cout << "Welcome to the US Geography Game!" << endl;
     cout << "Which mode would you like to play?" << endl;
     cout << "Capitols(1) or Cities(2)?" << endl;
+    cout << "0) to quit." << endl;
     cin >> choice;
 
+while(choice > 0){
     if(choice == 1){
         cout << "You chose the Capitol Option!" << endl;
+        cout << "Stop" << endl;
     }
     if(choice == 2){
         cout << "You chose the Cities Option!" << endl;
     }
 
-
+}
 
 
 
