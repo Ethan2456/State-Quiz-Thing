733a0ded62b10b706a3aceae7f770c082affbdf0
Daniel Bogaychuk <dbog@730-ln11701-r.jeffco.schools>
2023-11-29 08:22:31 -0700

    Helo

--- fun.cpp
+++ fun.cpp
@@ -25,7 +25,11 @@ int main(){
                 "Atlanta","Honolulu","Boise","Springfield","Indianapolis",
                 "Des Moines","Topeka","Frankfort","Baton Rouge","Augusta",
                 "Annapolis","Boston","Lansing","Saint Paul","Jackson",
-                "Jefferson City","Helena","Lincoln","Carson City","Concord"}
+                "Jefferson City","Helena","Lincoln","Carson City","Concord",
+                "Trenton","Santa Fe","Albany","Raleigh","Bismark","Columbus",
+                "Oklahoma City","Salem","Harrisburg","Providence","Columbia",
+                "Pierre","Nashville","Austin","Salt Lake City","Montpelier",
+                "Richmond","Olympia","Charleston","Madison","Cheyenne"};
 
     int choice;
     cout << "Welcome to the US Geography Game!" << endl;
