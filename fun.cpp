37ac0843766e44988539d59938dcd2806544e92d
Daniel Bogaychuk <dbog@730-ln11701-r.jeffco.schools>
2023-12-06 08:33:37 -0700

    hi fat boy

--- fun.cpp
+++ fun.cpp
@@ -61,7 +61,7 @@ int main(){
     int choice;
     cout << "Welcome to the US Geography Game!" << endl;
     cout << "Which mode would you like to play?" << endl;
-    cout << "Capitols(1) or Cities(2)?" << endl;
+    cout << "Capitals(1) or Cities(2)?" << endl;
     cout << "0) to quit." << endl;
     cout << "Your Option: ";
     cin >> choice;
